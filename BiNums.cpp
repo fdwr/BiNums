8b07f22b08036d4fe549896ef01b05b99d1bb438
Dwayne Robinson <fdwr@hotmail.com>
2022-10-26 20:15:50 -0700

    Ignore commas

--- BiNums.cpp
+++ BiNums.cpp
@@ -1894,6 +1894,10 @@ int ParseOperations(
                 isWithinParentheses = false;
                 break;
 
+            case Hash(","):
+                // Ignore commas.
+                break;
+
             default:
                 errorMessage = GetFormattedMessage("Unknown parameter: \"%.*s\"", int(param.size()), param.data());
                 return EXIT_FAILURE;
